e4d41f4244d4480ce26aede2597d3ea8123d808f
Ehsan Yousefi <mailbox5517@gmail.com>
2019-04-27 00:31:27 +0430

    first version

---
+++ Image.php
@@ -0,0 +1,106 @@
+<?php
+
+
+/* A library for uploading images safely with an object oriented approach for PHP 
+ * author Ehsan Yousefi 
+ * copyright 2019-2025 Ehsan Yousefi <mailbox5517@gmail.com> | https://fsdeveloper.ir 
+ * version 1.0.0     */
+class Image
+{
+    
+    //input assigned-name
+    public $name;
+
+    //system assigned temporary name
+    public $tmpName;
+    
+    public $allowedTypes = [];
+    
+    public $maxSize = 0;
+    
+    public $message;
+    
+    public function save($name, $directory, $file=null)
+    {
+        try{
+            $tmpName = $_FILES[$name]['tmp_name']; 
+            if($tmpName == null){
+                throw new exception('There is no uploaded file');
+                }
+            $this->tmpName = $tmpName;
+            $this->name = $name;
+       
+            /*All safty checking for the uploading file. */  
+            $this->secureimage();
+            $this->size();
+           }
+        catch (exception $e){
+            $this->message = $e->getMessage();
+            return 0;
+        }
+       
+        //if the new file name not inserted the original name will be used.
+        if($file == null){$file = $tmpName;}
+        //making the destination path from input parameters.
+        $filePath = $directory.'/'.$file;
+        //final and main operation.
+        return move_uploaded_file($tmpName, $filePath);
+    }   
+    protected function secureimage()
+    {
+        /*I am going to ckeck inside of the 
+        uploading file for any unwanted PHP script!
+        Please note that short php file start '<?' should be
+        turned off at php interpreter configs and should not be 
+        subject to search beacause many images include it!   */
+        $subject = "<?php";
+        $content = file_get_contents($this->tmpName);
+        $check = strpos($content , $subject);
+        if ($check) {
+             throw new exception('Script is not allowed');
+        }
+       
+        /*checking if the uploaded file is in compliance
+        with allowedTypes array in two following ways: */
+        if($this->allowedTypes != null){
+        
+            #1)checking the header of the uploaded file (week method).
+            $type = $_FILES["$this->name"]['type'];
+            $extractedEx = explode('/', $type);
+            $extractedEx = end($extractedEx); 
+            $check = in_array($extractedEx, $this->allowedTypes);
+            if(!$check){
+                throw new exception('This image type '.$extractedEx. ' is not allowed 0');
+            }
+            
+            #2)checking the extention of the uploaded file (more stronger method).
+            $fileName = $_FILES["$this->name"]['name'];
+            $ext = explode('.', $fileName);
+            $ext = end($ext);
+            $check = in_array ($ext, $this->allowedTypes);
+            if(!$check){
+                throw new exception('This image type '.$ext.' is not allowed 1');
+            }
+         }
+    }
+    protected function size()
+    {
+        if($this->maxSize != null){
+            $size = filesize($this->tmpName);
+            $maxSize = $this->maxSize;
+            if($maxSize < $size){
+                throw new exception('The image size '. $size.' bytes exceeded it\'s allowed value');
+            }
+        }
+    }
+    //use this method to return the error message or empty string. 
+    public function errorMessage()
+    {   
+        if($this->message != null){
+        return 'A message from image saving system:'.str_repeat('&nbsp;', 3).
+        "$this->message".'.'.str_repeat('&nbsp;', 2);
+        }else {
+        return "";
+        }
+    } 
+}
---
+++ README.md
@@ -0,0 +1,99 @@
+## Image  is a library for uploading images  
+
+\
+\
+## Introduction
+
+This minimal PHP library is my attempt to upload images safely by an object oriented approach.
+
+It's 79 lines.
+
+
+## Aims
+
+1- to provide safe uploading.
+
+2- to be fully in an object oriented approach.
+
+3- to have ability to define image name and image extension (to saved with),
+  
+  also maxSize, imageTypes, errorMassage.
+
+## How to use it
+
+
+
+#### parameters 
+
+This is the main method of Image:
+
+    save($name, $directory, $file)
+    
+**$name** 
+
+-Is the name that html form assigns to the uploaded image.
+
+**$directory**
+
+-Is the destination directory that the image will be saved in.
+
+-This should be an already existing directory on server.
+
+**$file**
+
+-Is the name and extention that you choose for the new uploaded file like personel.png  .
+
+-This third parameter can be omitted from the save() method.
+
+-If no $file value is provided, the client's uploaded name and extension will be used. 
+
+
+
+#### return value
+
+-class returns TRUE on success.
+
+-class returns FALSE on failure.
+
+
+
+#### image type and size configurations(object manipulation)
+
+-After instantiating the Image class and before using the save() method you can
+
+allocate values of $maxSize  :int (IN BYTES) and  $alowedTypes :array (of extensions only),
+ 
+not feeding them means accepting all sizes and all image types. 
+
+
+
+#### errorMessage3
+
+-Get and directly show the error message any where with using the errorMessage() method of the same object.
+
+-It returns NULL on a successful image saving.
+
+
+
+#### A Complete Example:
+\
+\
+    $obj = new Image  //-> or any other way like dependency injection
+
+    $obj -> maxSize = 4900000;
+    
+    $obj -> allowedTypes = ['png', 'jpg', 'jpeg']; 
+    
+    $obj -> save('image','pics/lastfolder','personel.png');
+    
+    echo ($obj -> errorMessage());
+\
+\
+## credit
+
+Developed by Ehsan Yousefi <mailbox5517@gmail.com> [https://fsdeveloper.ir]
+    
+## updates    
+    
+"20/4/2019 first release"
+
